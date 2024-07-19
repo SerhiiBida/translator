d655bef42057a4badabcfd7cae7f50756ea5b097
Sergii Bida <sergijbida15@gmail.com>
2024-07-19 19:12:38 +0300

    did basic translation

---
+++ js/history.js
@@ -0,0 +1,21 @@
+'use strict'
+
+export const addHistory = (inputText, outputText, inputLanguage, outputLanguage) => {
+    let history = {
+        records: []
+    };
+
+    if (localStorage.hasOwnProperty('history')) {
+        history = JSON.parse(localStorage.getItem('history'));
+    }
+
+    history.records.push({
+        inputText: inputText,
+        outputText: outputText,
+        inputLanguage: inputLanguage,
+        outputLanguage: outputLanguage,
+        date: new Date()
+    });
+
+    localStorage.setItem('history', JSON.stringify(history));
+}
--- js/translator.js
+++ js/translator.js
@@ -1,39 +1,44 @@
+'use strict'
+
+import {addHistory} from "./history.js";
+
 // Supported languages
 const languages = {
-    "en": "Англійська",
-    "ar": "Арабська",
-    "bn": "Бенгальська",
-    "vi": "В'єтнамська",
-    "nl": "Голландська",
-    "el": "Грецька",
-    "da": "Данська",
-    "es": "Іспанська",
-    "it": "Італійська",
-    "zh": "Китайська",
-    "ko": "Корейська",
-    "de": "Німецька",
-    "no": "Норвезька",
-    "pt": "Португальська",
-    "ru": "Російська",
-    "sw": "Суахілі",
-    "tr": "Турецька",
-    "uk": "Українська",
-    "fi": "Фінська",
-    "fr": "Французька",
-    "ja": "Японська"
+    'auto': 'Не знаю',
+    'en': 'Англійська',
+    'ar': 'Арабська',
+    'bn': 'Бенгальська',
+    'vi': `В'єтнамська`,
+    'nl': 'Голландська',
+    'el': 'Грецька',
+    'da': 'Данська',
+    'es': 'Іспанська',
+    'it': 'Італійська',
+    'zh': 'Китайська',
+    'ko': 'Корейська',
+    'de': 'Німецька',
+    'no': 'Норвезька',
+    'pt': 'Португальська',
+    'ru': 'Російська',
+    'sw': 'Суахілі',
+    'tr': 'Турецька',
+    'uk': 'Українська',
+    'fi': 'Фінська',
+    'fr': 'Французька',
+    'ja': 'Японська'
 };
 
-let inputLanguageCode = "en";
-let outputLanguageCode = "ja";
+let inputLanguageCode = 'en';
+let outputLanguageCode = 'ja';
 
 
 // Default languages lists
-const inputLanguagesList = document.querySelector(".selection-input-language-list");
-const outputLanguagesList = document.querySelector(".selection-output-language-list");
+const inputLanguagesList = document.querySelector('.selection-input-language-list');
+const outputLanguagesList = document.querySelector('.selection-output-language-list');
 
 const creatingListItems = (list, activeLanguageCode) => {
     for (let languageCode in languages) {
-        const item = document.createElement("option");
+        const item = document.createElement('option');
 
         item.textContent = languages[languageCode];
         item.value = languageCode;
@@ -46,17 +51,17 @@ const creatingListItems = (list, activeLanguageCode) => {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
     creatingListItems(inputLanguagesList, inputLanguageCode);
     creatingListItems(outputLanguagesList, outputLanguageCode);
 });
 
 
 // Language selection
-inputLanguagesList.addEventListener("change", function (event) {
+inputLanguagesList.addEventListener('change', function (event) {
     inputLanguageCode = event.target.value;
 });
-outputLanguagesList.addEventListener("change", function (event) {
+outputLanguagesList.addEventListener('change', function (event) {
     outputLanguageCode = event.target.value;
 });
 
@@ -80,7 +85,7 @@ const swapLanguages = (select, oldCode, newCode) => {
     });
 }
 
-swapButton.addEventListener("click", function () {
+swapButton.addEventListener('click', function () {
     swapLanguages(inputLanguagesList, inputLanguageCode, outputLanguageCode);
     swapLanguages(outputLanguagesList, outputLanguageCode, inputLanguageCode);
 
@@ -89,11 +94,70 @@ swapButton.addEventListener("click", function () {
 
 
 // Dynamic textarea
-const textInput = document.getElementById("translator-input");
+const textInput = document.getElementById('translator-input');
 
-textInput.addEventListener("input", function (event) {
+textInput.addEventListener('input', function (event) {
     const textarea = event.target;
 
-    textarea.style.height = "auto";  // Сброс высоты под текст
-    textarea.style.height = `${textarea.scrollHeight + 1}` + "px";
+    textarea.style.height = 'auto';  // Сброс высоты под текст
+    textarea.style.height = `${textarea.scrollHeight + 1}` + 'px';
+});
+
+
+// Translation
+const RAPID_API_KEY = 'e42deb1a7amsh39fe1a63df9f61cp192bfbjsn4378f1d52d1e';
+const RAPID_API_HOST = 'simple-translate2.p.rapidapi.com';
+
+const inputTextarea = document.getElementById('translator-input');
+const outputTextarea = document.getElementById('translator-output');
+
+let translationTimer;
+
+const getTranslation = async (text) => {
+    const url = `https://simple-translate2.p.rapidapi.com/translate?source_lang=${inputLanguageCode}&target_lang=${outputLanguageCode}`;
+
+    const sourceText = {
+        sourceText: text
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-rapidapi-key': RAPID_API_KEY,
+            'x-rapidapi-host': RAPID_API_HOST,
+        },
+        body: JSON.stringify(sourceText)
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        return result['data']['targetText'];
+    } catch (error) {
+        return 'Error...';
+    }
+}
+
+const translator = async () => {
+    const text = inputTextarea.value.trim();
+
+    if (!text) {
+        return;
+    }
+    const translatedText = await getTranslation(text);
+
+    addHistory(text, translatedText, inputLanguageCode, outputLanguageCode);
+
+    outputTextarea.value = translatedText;
+}
+
+inputTextarea.addEventListener('input', function () {
+    // Сброс
+    clearTimeout(translationTimer);
+
+    outputTextarea.value = '...';
+
+    translationTimer = setTimeout(translator, 1500);
 });
