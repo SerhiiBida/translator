049da787c72d7c1febb292635097f3064954344f
Sergii Bida <sergijbida15@gmail.com>
2024-07-24 12:10:26 +0300

    did change theme

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
 'use strict'
+
+
+const changeThemeButton = document.querySelector('.site-theme-button');
+
+const imgThemeDark = document.querySelector('.site-theme-dark');
+const imgThemeLight = document.querySelector('.site-theme-light');
+
+// Default theme
+document.addEventListener('DOMContentLoaded', function () {
+    if (!localStorage.hasOwnProperty('defaultTheme')) {
+        localStorage.setItem('defaultTheme', 'light');
+
+        imgThemeDark.classList.add('show-img');
+        return;
+    }
+
+    const defaultTheme = localStorage.getItem('defaultTheme');
+
+    if (defaultTheme === 'dark') {
+        document.body.classList.add('dark');
+        imgThemeLight.classList.add('show-img');
+    } else {
+        imgThemeDark.classList.add('show-img');
+    }
+})
+
+// Change  theme
+changeThemeButton.addEventListener('click', function () {
+    document.body.classList.toggle('dark');
+
+    if (document.body.classList.contains('dark')) {
+        imgThemeLight.classList.add('show-img');
+        imgThemeDark.classList.remove('show-img');
+
+        localStorage.setItem('defaultTheme', 'dark');
+        return;
+    }
+
+    imgThemeDark.classList.add('show-img');
+    imgThemeLight.classList.remove('show-img');
+
+    localStorage.setItem('defaultTheme', 'light');
+});
